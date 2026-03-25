edb52c131cc554983e01524d300fd9ad9992c16f
Hong Le Minh Thong <hl.minhthong2006@gmail.com>
2026-03-25 15:11:49 +0700

    ???

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,9 @@ use App\Http\Controllers\Admin\lichlaythuController;
 use Illuminate\Support\Facades\Route;
 use App\Services\QL;
 use App\Http\Controllers\AdminAuthController;
-use App\Http\Controllers\Admin\QLGoiBaoDuong_controller;
+use App\Http\Controllers\User\BaoDuong_controller;
 use App\Http\Controllers\Admin\QLBaoDuong_controller;
+use App\Http\Controllers\Admin\QLGoiBaoDuong_controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -91,9 +92,26 @@ Route::get('/trang_admin/lai_thu/xoa/{id}', [lichLayThuController::class, 'xoa']
 
    });
 
+   
+   // BẢO DƯỠNG XE
+Route::get('/car_shop/baoduong', function () {
+    return view('user.layouts.BaoDuong');
+})->name('baoduong');
+Route::get('/car_shop/baoduong', [BaoDuong_controller::class, 'trang_baoduong']);
+Route::post('/car_shop/datlichbaoduong', [BaoDuong_controller::class, 'datlich_baoduong']);
+
+// BẢO DƯỠNG ADMIN
+Route::get('/trang_admin/baoduong', [QLBaoDuong_controller::class, 'index']);
+
+// GÓI BẢO DƯỠNG
+Route::get('/trang_admin/goibaoduong', [QLGoiBaoDuong_controller::class,'index']);
+Route::get('/trang_admin/goibaoduong/them', function () {
+    return view('admin.layouts.index_AD', [
+        'key' => 'add_goi_bao_duong'
+    ]);
+
+    Route::get('/trang_admin/goibaoduong/xoa/{id}', [QLGoiBaoDuong_controller::class,'xoa_goi']);
+});
+
 
    
-
-
-
-
